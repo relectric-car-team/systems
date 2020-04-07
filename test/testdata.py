2485c1dfcbacde8f4e7df58248150a87ba5698a1
elizetran <54915362+elizetran@users.noreply.github.com>
2020-04-07 12:12:50 -0600

    Update testdata.py

--- test/testdata.py
+++ test/testdata.py
@@ -1,9 +1,13 @@
 class TestData():
+
+# change test data file
+CONST_FILE_NAME = "testdata.csv"
   def loadData(self) -> None:
-    pass
+    file = open(CONST_FILE_NAME, "r")
 
   def update(self) -> None:
-    pass
+    file = open(CONST_FILE_NAME, "w")
+    
 
   def get(self, name: str) -> None:
     pass
