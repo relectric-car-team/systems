d58fc04b3a9198253cb38a50cc43ffa5e6e5fe87
Jayden <jayden.bischoff1@ucalgary.ca>
2020-04-16 10:24:34 -0600

    Fixed Systems class functions

--- systems.py
+++ systems.py
@@ -5,34 +5,43 @@ from controllers import *
 
 class Systems():
   
-  # Creates and initializes all of the controllers
+  """ Creates and initializes all of the controllers
+  """
   def __init__(self):
     self.controllers = [];
-    controllers[0] = MotorController()
-    controllers[1] = BatteryController()
-    controllers[2] = ClimateController()
-    controllers[3] = SensorController()
-    controllers[4] = BackupController()
+    self.controllers.append(MotorController())
+    self.controllers.append(BatteryController())
+    self.controllers.append(ClimateController())
+    self.controllers.append(SensorController())
+    self.controllers.append(BackupController())
 
-  # Finds and returns the value of the specified variable in a certain controller
+  """ Finds and returns the value of the specified variable in a certain controller
+  """
   def get(name: str) -> any:
     for i in controllers:
       value = controllers[i].getVariable(controllers[i], name)
-      return value
+      if value is not None:
+        return value
+    return None
 
-  # Finds and sets the value of the specified variable in a certain controller
+  """ Finds and sets the value of the specified variable in a certain controller
+  """
   def set(name: str, value: any) -> None:
     for i in controllers:
       if name in controllers[i].variables:
-        controllers[i].setVariable(name, value)  
+        controllers[i].setVariable(name, value) 
+        return
 
-  # Calls the action specified by a controller with the provided arguments
+  """ Calls the action specified by a controller with the provided arguments
+  """
   def sendAction(name: str, args: Tuple[any]) -> None:
     for i in controllers:
       if name in controllers[i].actions:
         controllers[i].performAction(controllers[i], name, args)
+        return
         
-  # Shuts the controllers down
+  """ Shuts the controllers down
+  """
   def shutdown(self) -> None:
     for i in controllers:
       controllers[i].shutdown()
