51ebf246271edb8d107291d331de361c8aa43194
Sonic7662 <madrocker2000@gmail.com>
2020-04-22 20:31:25 -0600

    Initial version of NetworkManager. At this point in time the class is fairly simple and hands off, requiring its users to maintain the network interfaces. In the future, in addition to implementing CANBusNet as a network interface, this class should take some of the responsibility of maintaining the various connections.

--- networkmanager.py
+++ networkmanager.py
@@ -1,7 +1,9 @@
 # Imports
+from typing import List
+
 from net import PiNet, ArduinoNet
 
-""" NetworkManager takes ownsership of all networking library objects so that
+""" NetworkManager takes ownership of all networking library objects so that
 	instances of controller classes may use them. Passing this class down to the
 	controller classes ensures they do not use duplicates of these networking
 	interfaces.
@@ -9,21 +11,40 @@ from net import PiNet, ArduinoNet
 class NetworkManager():
 	""" Initializes NetworkManager and creates and starts all of the network
 		interfaces.
+
+		serial_ports - A list containing the platform identifiers (strings) for the
+			serial ports to be bound. The index of the identifier is the same as the
+			integer used to retrieve a the respective ArduinoNet instance with
+			getArduinoNet(). Example identifiers include 'COM3' for Windows and
+			'/dev/ttyUSB0' for GNU/Linux.
 	"""
-	def __init__(self):
-		self.__pinet = PiNet(True, ("localhost", 4000))
+	def __init__(self, serial_ports: List[str]) -> None:
+		self.__pinet = PiNet(True, ("localhost", 4000)) # Assuming we use this port
+		self.__arduinonets = []
+		for i in serial_ports:
+			self.__arduinonets.append(ArduinoNet(i))
 
 	""" Returns the active PiNet instance.
 	"""
 	def getPiNet(self) -> PiNet:
-		pass
+		return self.__pinet
 
 	""" Returns the active ArduinoNet instance.
+	port - An integer specifying the serial port tied to the ArduinoNet instance
+		being requested. See __init__().
 	"""
 	def getArduinoNet(self, port: int) -> ArduinoNet:
-		pass
+		return self.__arduinonet
+
+	""" Returns the active CANBusNet instance.
+	"""
+	# ==== The following is commented until a template of CANBusNet can be made.
+	#def getCANBusNet(self) -> CANBusNet:
+	#	pass
 
 	""" Safely closes all network interfaces.
 	"""
 	def stop(self) -> None:
-		pass
+		self.__pinet.stop()
+		for port in self.__arduinonets:
+			port.stop()
