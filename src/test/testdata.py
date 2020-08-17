dac3ed45ee5c739dbfc11da14f481a5f1a3e39ec
vincentz6 <55777027+vincentz6@users.noreply.github.com>
2020-08-17 17:18:27 -0600

    Update testdata.py

--- src/test/testdata.py
+++ src/test/testdata.py
@@ -1,22 +1,19 @@
 import pandas as pd  # pip install pandas
+import sys
 
 # Constants
-CONST_FILE_NAME = "testdata.csv"
-col_list = []
-
-df = pd.read_csv(CONST_FILE_NAME, skiprows = 1) #read the csv file, skipping the headers
 
 class TestData:
     """ TestData accesses the file of mock data to send to other classes in
     place of any engine, battery, sensor, etc. data.
     """
     
-    def __init__(self):
+    def __init__(self, file_path):
         """ Initializes TestData variables which are all potential data that
         could be	sent from other functions of the vehicle. Time is set to 0
         to begin.
         """
-        pd.read_csv(CONST_FILE_NAME, nrows = 1).col_list.tolist() #read the headers of the controller and store into a list
+        self.df = pd.read_csv(file_path)
         self.time = 0
 
     def update(self) -> None:
@@ -30,6 +27,4 @@ class TestData:
         return the current data as dictated by current time value.
         """
 
-        for i in col_list: #loop through header names
-            if name == i:
-                return df[i][self.time] #if the argument name matches the header, return the value of the argument at given time
+        return self.df[name][self.time]
