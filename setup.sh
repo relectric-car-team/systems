d60ded545ca111452ddea425dbd25f892643e599
Ratik Kapoor <ratikrk@gmail.com>
2021-03-11 15:09:58 -0700

    chore: Docker Python 3.9.2, container extensions

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 # !/bin/sh
 
 if ! [ -x "$(command -v poetry)" ]; then
-    echo "bruh install poetry then come back"
+    echo "Please see README instructions for Getting Started"
     exit 1
 else
     poetry install
     pre-commit install
-    echo "this one might take some time for initial setup"
+    echo "Setting up pre-commit, this may take a while"
     pre-commit run --all-files
-    echo "setup completed"
+    echo "Setup completed!"
     exit 0
 fi
