96d2966f0ca562cd30157160dac296b8a68811bb
dhyey <dhyeylalseta@outlook.com>
2021-03-15 00:38:09 -0600

    fix: pre-commit tweak, type validation tweak, test
    * small python type string tweak
    
    * setup tests for the easiest module in systems

---
+++ tests/systems/test_controllers.py
@@ -0,0 +1,38 @@
+import pytest
+
+from systems import controllers
+
+
+@pytest.fixture
+def dummy_controller():
+    Controller = controllers.ControllerDecorator()
+
+    @Controller
+    class DummyController:
+        attr1: int = 0
+        attr2: str = "attr2"
+
+    return DummyController()
+
+
+def test_controller_decorator_getters(dummy_controller):
+    assert dummy_controller['attr1'] == 0
+    assert dummy_controller['attr2'] == "attr2"
+    assert dummy_controller.asdict() == {'attr1': 0, 'attr2': "attr2"}
+
+
+def test_controller_decorator_setters(dummy_controller):
+    dummy_controller['attr1'] = 5
+    dummy_controller['attr2'] = "second attr"
+
+    assert dummy_controller['attr1'] == 5
+    assert dummy_controller['attr2'] == "second attr"
+    assert dummy_controller.asdict() == {'attr1': 5, 'attr2': "second attr"}
+
+
+def test_controller_decorator_type_validation(dummy_controller):
+    with pytest.raises(TypeError):
+        dummy_controller['attr1'] = "string on int"
+
+    with pytest.raises(TypeError):
+        dummy_controller['attr2'] = 4
